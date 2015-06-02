a9332b52514faa8b04bbb9ab5a63df683c5c5056
Rinesh Ramadhin <Rinesh.Ramadhin@gmail.com>
2015-06-02 11:19:36 +0200

    Added postgres script

---
+++ Advanced Databases/Week 4/CREATE Roles-User-Views.sql
@@ -0,0 +1,37 @@
+﻿DROP OWNED BY student_role;
+REVOKE ALL ON student FROM student_role;
+DROP USER student_user;
+DROP ROLE student_role;
+
+CREATE ROLE student_role;
+GRANT CONNECT ON DATABASE postgres TO student_role;
+GRANT USAGE ON SCHEMA public TO student_role;
+GRANT SELECT ON student TO student_role;
+GRANT SELECT ON INF0D_view TO student_role;
+GRANT SELECT ON INF1D_view TO student_role;
+GRANT SELECT ON INF2D_view TO student_role;
+GRANT SELECT ON INF3D_view TO student_role;
+GRANT SELECT ON INF4D_view TO student_role;
+
+CREATE USER student_user WITH PASSWORD '1234';
+GRANT student_role to student_user;
+
+CREATE OR REPLACE VIEW INF1D_view AS
+SELECT * FROM student WHERE klas = 'INF1D';
+
+
+CREATE OR REPLACE VIEW INF2D_view AS
+SELECT * FROM student WHERE klas = 'INF2D';
+
+
+CREATE OR REPLACE VIEW INF3D_view AS
+SELECT * FROM student WHERE klas = 'INF3D';
+
+
+CREATE OR REPLACE VIEW INF4D_view AS
+SELECT * FROM student WHERE klas = 'INF4D';
+
+
+CREATE OR REPLACE VIEW INF0D_view AS
+SELECT * FROM student WHERE klas = 'INF0D';
+
---
+++ Advanced Databases/Week 4/CREATE table MET ROLLEN en VIEWS.sql
@@ -0,0 +1,25 @@
+﻿-- Database: postgres
+
+-- DROP DATABASE postgres;
+
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
+CREATE DATABASE postgres
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'English_United States.1252'
+       LC_CTYPE = 'English_United States.1252'
+       CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE postgres
+  IS 'default administrative connection database';
+
+CREATE TABLE Student(
+	  studentnummer                VARCHAR(7)   NOT NULL  PRIMARY KEY  CHECK (length(studentnummer) = 7)
+	, wachtwoord                   VARCHAR(45)  NOT NULL
+	, naam			       VARCHAR(45)  NOT NULL
+	, klas		               VARCHAR(45)
+	, ingeschreven		       BOOLEAN      NOT NULL 
+);
