9418ce22d0518a67d47e9d12bbb9f9792685238e
Hoye <hoyelam96@gmail.com>
2015-06-22 21:26:03 +0200

    sync

--- Advanced Databases/Week 5/Examples/HibernateMavenExample/HibernateMavenExample/src/main/java/entities/Categorie.java
+++ Advanced Databases/Week 5/Examples/HibernateMavenExample/HibernateMavenExample/src/main/java/entities/Categorie.java
@@ -1,51 +1,68 @@
 package entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
 public class Categorie {
-	
+	private Integer idCategorie;
 	private String categorieNaam;
-	private Advertentie advertentie;
-	private Categorie categorie;
+	private Categorie subCategorie;
+	private Set<Categorie> sub = new HashSet<Categorie>();
 	
-	public Categorie getCategorie() {
-		return categorie;
-	}
-
-
-	public void setCategorie(Categorie categorie) {
-		this.categorie = categorie;
-	}
-
-
 	public Categorie(String categorieNaam) {
 		this();
 		this.categorieNaam = categorieNaam;
 	}
 
+	public Categorie() {
+	}
 
+	@Id
+	@GeneratedValue
+	@Column(name="idCategorie")
+	public Integer getIdCategorie() {
+		return idCategorie;
+	}
+
+	public void setIdCategorie(Integer idCategorie) {
+		this.idCategorie = idCategorie;
+	}
+
+	@Column(name="categorieNaam")
 	public String getCategorieNaam() {
 		return categorieNaam;
 	}
-
-
 
 	public void setCategorieNaam(String categorieNaam) {
 		this.categorieNaam = categorieNaam;
 	}
 
-
-
-	public Advertentie getAdvertentie() {
-		return advertentie;
+	@ManyToOne(cascade = {CascadeType.ALL})
+	@JoinColumn(name="idSubCategorie")
+	public Categorie getSubCategorie() {
+		return subCategorie;
 	}
 
-
-
-	public void setAdvertentie(Advertentie advertentie) {
-		this.advertentie = advertentie;
+	public void setSubCategorie(Categorie subCategorie) {
+		this.subCategorie = subCategorie;
 	}
 
-
-
-	public Categorie() {
+	@OneToMany(mappedBy="subCategorie")
+	public Set<Categorie> getSub() {
+		return sub;
 	}
+
+	public void setSub(Set<Categorie> sub) {
+		this.sub = sub;
+	}
+	
+	
 }
